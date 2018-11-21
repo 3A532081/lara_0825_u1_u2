61aa4203e544c6ef76a874c35e4ed1db0a3798fd
3A532081 <tanshui601@gmail.com>
2018-11-21 16:21:10 +0800

    使用update方法

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,13 @@ Route::get('/', function () {
     /*$post=\App\Post::find(1);
     dd($post);*/
 
-    $posts=\App\Post::where('id','<',10)->orderBy('id','DESC')->get();
-    dd($posts);
+    /*$posts=\App\Post::where('id','<',10)->orderBy('id','DESC')->get();
+    dd($posts);*/
+
+    $post=\App\Post::find(1);
+    $post->update([
+        'title'=>'update dtitle',
+        'content'=>'updated content',
+    ]);
+
 });
